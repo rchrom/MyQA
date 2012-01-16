067422d54ff8b3d5d9ec2c7d47d0cbe5258048c4
Radek Chromy <radek.chromy@gooddata.com>
2012-01-16 17:14:00 +0100

    added comments

--- copy_hudson_cfg/copy_hudon_config.pl
+++ copy_hudson_cfg/copy_hudon_config.pl
@@ -1,16 +1,22 @@
 use strict;
 use File::Copy;
 
+######################################
+# Hudson configuration sources
 my $hudson_dir="/var/lib/hudson";
+
+# Destination folder where to copy configuraiton 
+my $outputdir="/tmp/ra";
+
+#############################################
+
 my $hudson_jobs_dir="$hudson_dir/jobs";
 my $file;
-my $outputdir="/tmp/ra";
+my $config="config.xml";
 
 mkdir $outputdir;
 
-
-my $config="config.xml";
-
+# copy config.xml file from directory into directory
 sub copyconfig {
 	my $fromdir = shift;
 	my $todir = shift;
@@ -19,8 +25,9 @@ sub copyconfig {
 	
 	mkdir ($todir);
 	copy("$fromdir/$config",$todir) or die "File cannod be copied $fromdir/$config";
-
 }
+
+
 
 opendir ( DIR, $hudson_jobs_dir) || die "Error in openning dir $hudson_jobs_dir \n";
 
@@ -30,6 +37,7 @@ while ($file = readdir(DIR)){
 	# skip .* and files
 	next if grep { /^\./ || -f "$hudson_jobs_dir/$file"} $file ; 
 	print $file."\n";
+	#  copy config file per job
 	copyconfig ("$hudson_jobs_dir/$file","$outputdir/$file");
 }
 closedir DIR;
