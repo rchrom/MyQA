37dd2c520fe8144fc3498fe30472bab4cc3db67e
Radek Chromy <radek.chromy@gooddata.com>
2012-02-01 14:54:33 +0100

    initial

---
+++ BambooTest/src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author radek
+ *
+ */
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * sysout
+		 */
+		System.out.println("Hello World");
+	}
+}
---
+++ BambooTest/src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testHello() {
+		HelloWorld.main(null);
+		assertTrue(true);
+	}
+
+}
